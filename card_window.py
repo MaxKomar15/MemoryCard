30a61ec77eddb3998555a2e93e3cfa5469dc6a33
MaxKomar15 <143327983+MaxKomar15@users.noreply.github.com>
2023-10-01 15:37:01 +0300

    Add files via upload

--- card_window.py
+++ card_window.py
@@ -1,66 +1,64 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QGroupBox, QButtonGroup, QRadioButton, QHBoxLayout, QMessageBox, QLabel, QVBoxLayout, QPushButton, QSpinBox
+from PyQt5.QtWidgets import QRadioButton, QHBoxLayout, QMessageBox, QWidget, QLabel, QPushButton, QVBoxLayout, QSpinBox, QGroupBox, QButtonGroup
 
-menu_btn = QPushButton("Меню")
-sleep_btn = QPushButton("Відпочити")
+menu_btn = QPushButton('Меню')
+sleep_btn = QPushButton('Відпочити')
 time_box = QSpinBox()
-time_box.setValue(30)
-
 time_lb = QLabel("хвилин")
 
+time_box.setValue(3)
+
+
 line1 = QHBoxLayout()
+
+
 line1.addWidget(menu_btn)
 line1.addStretch(1)
 line1.addWidget(sleep_btn)
 line1.addWidget(time_box)
 line1.addWidget(time_lb)
 
-question = QLabel("Яблуко")
+question = QLabel('Яблуко')
 
-btn1 = QRadioButton("apply")
-btn2 = QRadioButton("apple")
-btn3 = QRadioButton("appla")
-btn4 = QRadioButton("applu")
+btn1 = QRadioButton('Apple')
+btn2 = QRadioButton('apply')
+btn3 = QRadioButton('pear')
+btn4 = QRadioButton('yabluko')
 
-group_box = QGroupBox("Варіанти відповідей:")
+group_box = QGroupBox("Варіанти перекладу:")
 radio_group = QButtonGroup()
+
 radio_group.addButton(btn1)
 radio_group.addButton(btn2)
 radio_group.addButton(btn3)
 radio_group.addButton(btn4)
 
-
-col1 = QHBoxLayout()
-col2 = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
 line2 = QHBoxLayout()
 
 col1.addWidget(btn1)
 col1.addWidget(btn2)
-col1.addWidget(btn3)
-col1.addWidget(btn4)
+col2.addWidget(btn3)
+col2.addWidget(btn4)
 
 line2.addLayout(col1)
 line2.addLayout(col2)
-group_box.setLayout(line2)
 
-answer_btn = QPushButton("Відповісти")
+answer_btn = QPushButton('Відповісти')
+answer_box = QGroupBox("Результат тесту")
+result_lb = QLabel("Правильно")
+right_lb = QLabel("Правильна Відповідь")
 
-answer_box =QGroupBox("Результати тесту:")
-
-result_lb =QLabel("Правильно")
-right_lb =QLabel("Правильна відповідь")
-
-res_line =QVBoxLayout()
+res_line = QVBoxLayout()
 res_line.addWidget(result_lb, alignment=(Qt.AlignLeft|Qt.AlignTop))
-res_line.addWidget(right_lb, alignment= Qt.AlignCenter, stretch= 2)
-
+res_line.addWidget(right_lb, alignment=Qt.AlignCenter, stretch= 2)
 answer_box.setLayout(res_line)
 answer_box.hide()
-
-
 main_line = QVBoxLayout()
-main_line.addLayout(line1, stretch=1)
-main_line.addWidget(question, alignment=Qt.AlignCenter, stretch=2)
+main_line.addLayout(line1)
+main_line.addWidget(question, alignment=Qt.AlignCenter, stretch=1 )
+group_box.setLayout(line2)
 main_line.addWidget(group_box, stretch=8)
 main_line.addWidget(answer_box, stretch=8)
 main_line.addWidget(answer_btn, stretch=3)
--- main.py
+++ main.py
@@ -1,42 +1,80 @@
-from random import choice, shuffle
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QRadioButton, QHBoxLayout, QMessageBox, QLabel, QVBoxLayout
+from time import sleep
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from random import choice, shuffle
 
 app = QApplication([])
 
-from card_window import *
+from card_window import:
+from menu_window import:
 
+from card_window import*
 class Question:
     current = None
+    count_right = 0
+    count_wrong = 0
+
+
     def __init__(self, question, right_answer, wrong_answer1, wrong_answer2, wrong_answer3):
-        self.question = question
+        self.question = question 
         self.right_answer = right_answer
         self.wrong_answer1 = wrong_answer1
         self.wrong_answer2 = wrong_answer2
         self.wrong_answer3 = wrong_answer3
 
-q1 = Question("Собака", "Dog", "Hot-Dog", "Tog", "Dok")
-q2 = Question("Холодильник", "frige", "Fritqe", "trich","fritch")
-q3 = Question("Кіт", "Cat", "Kat", "Cad", "Dad")
-q4 = Question("Вікно", "window", "vindow", "findow", "Hindow")
+    def got_right(self):
+        Question.count_ans += 1
+        Question.count_right += 1
 
-questions = [q1, q2, q3, q4]
+    def got_wrong(self):
+        Question.count_ans += 1
+
+q1 = Question('Людина', 'people', 'peple', 'lubunna', 'neger')
+q2 = Question("Комп'ютер", 'computer', 'Lapton', 'comp', 'comlp')
+q3 = Question('число', 'number', 'nomer', 'chuslo', 'calm')
+questions = [q1,q2, q3]
+radio_buttons = [btn1, btn2, btn3, btn4]
+
+
+
 
 main_win = QWidget()
-main_win.setWindowTitle("Memory Card")
+main_win.setWindowTitle('Memory Card')
 main_win.resize(600, 500)
-main_win.move(300, 150)
+main_win.move(150, 100)
 main_win.setLayout(main_line)
 
-
-def new_question():
+def new_qustion():
     Question.current = choice(questions)
     question.setText(Question.current.question)
     right_lb.setText(Question.current.right_answer)
+    shuffle(radio_buttons)
+    radio_buttons[0].setText(Question.current.right_answer)
 
-new_question()
+    radio_buttons[1].setText(Question.current.wrong_answer1)
+    radio_buttons[2].setText(Question.current.wrong_answer2)
+    radio_buttons[3].setText(Question.current.wrong_answer3)
+
+    
+
+new_qustion()
+
+def check_result():
+    radio_group.setExclusive(False)
+    if radio_buttons[0].isChecked():
+        Question.count_right()
+        result_lb.setText("Правильно")
+        radio_group[0].isChecked(False)
+    else:
+        Question.count_wrong()
+        result_lb.setText("Неправильно")
+
+    radio_group.setExclusive(True)
+
+
 def click_btn():
-    if  answer_btn.text() == "Відповісти":
+    if answer_btn.text() == "Відповісти":
+        check_result()
         group_box.hide()
         answer_box.show()
         answer_btn.setText("Наступне питання")
@@ -44,11 +82,26 @@ def click_btn():
         answer_box.hide()
         group_box.show()
         answer_btn.setText("Відповісти")
-        new_question()
+        new_qustion()
 
-answer_btn.clicked.connected(click_btn)
+answer_btn.clicked.connect(click_btn)
+
+def relax():
+    pause_time = int(time_box.value()) * 60
+    main_win.hide()
+    sleep(pause_time)
+    main_win.show()
+
+def menu_show():
+    main_win.hide()
+    count_ans_lb.setText(f"Кількісь відповідей:(Question.count_ans)")
+    count_right_lb.setText(f"Кількісь відповідей:(Question.count_right)")
+    secess_lb.setText(f"Успішність: {2/4*100}")
+    menu_win.show()
+    
+answer_btn.clicked.connect(relax)
+sleep_btn.clicked.connect(relax)
+menu_btn.clicked.connect(menu_show)
 
 main_win.show()
 app.exec_()
-
-
